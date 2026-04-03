2ffa9be84cdd31beb4cf6c4458c99d22c47f4379
SangHyun Byun <101382267+ShByeon3968@users.noreply.github.com>
2026-04-03 10:50:38 +0900

    초기 커밋.

---
+++ VDashPro/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Prism.Ioc;
+using Prism.DryIoc;
+using VDashPro.Views;
+
+namespace VDashPro
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+
+}
---
+++ VDashPro/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace VDashPro.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "V-Dash Pro";
+    }
+}
